9f7b3b31093cf9d689cb381c3fb9c9312aa347de
justinr <justin@apsis.co>
2021-03-20 07:58:22 -0700

    first iteration of squares.c

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -8,8 +8,13 @@
  */
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
+	if ( coord >= offset && coord < offset + size ) {
+		return 1;
+	}
   // else, return 0
-  return 0;
+	else {
+		return 0;
+	}
 }
 
 /*
@@ -19,17 +24,71 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
-  return 0;
+	if (coord == offset || coord == offset + size ) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+void condition1(int x, int y, int x_offset, int y_offset, int size2) {
+
+	if ( (x >= x_offset && x < x_offset + size2) && (y == y_offset || y == y_offset + size2 - 1) ){
+		return true;
+	}
+	else if ( (y >= y_offset && y < y_offset + size2) && (x == x_offset || x == x_offset + size2 - 1) ){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void condition2(x, y, size1) {
+
+      // check if EITHER
+      //    x is less than size1 AND (y is either 0 or size1-1)
+      // OR
+      //    y is less than size1 AND (x is either 0 or size1-1)
+
+	if ( x < size1 && (y == 0 || y == size1-1) ) {
+		return true;
+	} else if (y < size1 && (x == 0 || x == size1-1)) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+void printPound(int x, int y, int size1) {
+	
+
 }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
+	if (size1 > x_offset + size2) {
+		w = size1;
+	} else {
+		w = x_offset + size2;
+	}
 
   //compute the max of size1 and (y_offset + size2).  Call this h
+	if (size1 > y_offset + size2) {
+		h = size1;
+	} else {
+		h = y_offset + size2;
+	}
 
   //count from 0 to h. Call the number you count with y
+	for ( y = 0; y < h; y++ ) {
+		
+	}
 
     //count from 0 to w. Call the number you count with x
+	for ( x = 0; x < w; x++ ) {
+
 
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
@@ -38,6 +97,14 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
+      
+	bool condition1=condition1(x, y, x_offset, y_offset, size2);
+	bool condition2=condition2(x, y, size1);
+
+	if (condition1 == true) {
+		printf("*");
+	}
+
 
       //if not,
       // check if EITHER
@@ -46,8 +113,18 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
 
+	else if (condition2 == true) {
+		printf("#");
+	}
+
       //else print a space
+	else {
+		printSpace(x, y, size1);
+	}
+
+	}
     //when you finish counting x from 0 to w, 
     //print a newline
+    printf("\n");
 
 }
